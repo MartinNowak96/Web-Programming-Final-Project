f5fb5c47fe1a6de2fef9aac0ea9649e15e38430e
Martin Nowak <martinnowak96@outlook.com>
2018-09-09 12:21:25 -0400

    added rotate, added buttons under tables

--- final/src/employeeTables.js
+++ final/src/employeeTables.js
@@ -1,14 +1,17 @@
-import {GridOptions, GridApi, ColumnApi} from "ag-grid-community";
-import {inject} from 'aurelia-framework'
+import { GridOptions, GridApi, ColumnApi } from "ag-grid-community";
+import { inject } from 'aurelia-framework'
 
-export class employeeTable{
+export class employeeTable {
     mainEmployeeGridOptions = GridOptions
     gridApi = GridApi
     columnApi = ColumnApi;
 
-    constructor(){
+    constructor() {
         //console.log(this.gridOptions)
-        this.employeeRowData =[{ name:"Martin Nowak", phone:"555-555-5555", address:"180 Smith Street, Middletown NY, 10940" }]
+
+        this.isHalf = false;
+        this.employeeRowData = [{ name: "Martin Nowak", phone: "555-555-5555", address: "180 Smith Street, Middletown NY, 10940", deployed:"Yes" }]
+
 
         this.mainEmployeeGridOptions = {
             rowData: this.employeeRowData,
@@ -23,11 +26,32 @@ export class employeeTable{
     }
 
     attached() {
-        
-        
+
+
+
     }
 
-    onReady(e){
+    rotateTables() {
+        if (this.isHalf == false) {
+            employeeWrapper.classList.remove("col")
+            employeeWrapper.classList.add("col-6")
+
+            groupWrapper.classList.remove("col")
+            groupWrapper.classList.add("col-6")
+            tableWrapper.classList.add("row")
+            this.isHalf = true;
+        } else {
+            this.isHalf = false
+            employeeWrapper.classList.remove("col-6")
+            employeeWrapper.classList.add("col")
+
+            groupWrapper.classList.remove("col-6")
+            groupWrapper.classList.add("col")
+            tableWrapper.classList.remove("row")
+        }
+    }
+
+    onReady(e) {
         console.log("hi")
         console.log(e)
         console.log(this.mainEmployeeGridOptions)
